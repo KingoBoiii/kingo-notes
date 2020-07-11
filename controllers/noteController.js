44aef5b48e28f4771e0469b0f0209675c74eca24
KingoBoiii <kingodk@outlook.dk>
2020-07-11 22:06:28 +0200

    added details page, and delete function

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -7,6 +7,17 @@ const note_index = function(req, res) {
         }).catch(err => console.log(err));
 }
 
+const note_details = (req, res) => {
+    const id = req.params.id;
+    Note.findById(id).then((result) => {
+
+        res.render('notes/details.ejs', { title: 'Note', note: result });
+    }).catch((err) => {
+        res.status(404).render('404', { title: 'Note not found' });
+        console.log(err);
+    });
+}
+
 const note_create_get = (req, res) => {
     res.render('notes/create.ejs', {
         title: 'Create a Note'
@@ -22,8 +33,19 @@ const note_create_post = (req, res) => {
     });
 }
 
+const note_delete = (req, res) => {
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id).then((result => {
+        res.json({ redirect: '/notes' });
+    })).catch(err => {
+        console.log(err);
+    });
+}
+
 module.exports = {
     note_index,
+    note_details,
     note_create_get,
-    note_create_post
+    note_create_post,
+    note_delete
 }
--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -6,5 +6,7 @@ const noteController = require('../controllers/noteController');
 router.get('/notes', noteController.note_index);
 router.post('/notes', noteController.note_create_post);
 router.get('/create', noteController.note_create_get);
+router.get('/notes/:id', noteController.note_details);
+router.delete('/notes/:id', noteController.note_delete);
 
 module.exports = router;
