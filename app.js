373287a34d57a26b638d4876cdca94d03def7816
KingoBoiii <kingodk@outlook.dk>
2020-07-11 18:38:53 +0200

    Added Materialize, Navbar and about page

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ const morgan = require('morgan');
 
 app.set('view engine', 'ejs');
 
+app.use(express.static('public'));
 app.use(morgan('dev'));
 
 app.get('/', function(req, res) {
-    res.render('index.ejs');
+    res.render('index.ejs', { title: 'Notes' });
+});
+
+app.get('/about', function(req, res) {
+    res.render('about.ejs', { title: 'About' });
 });
 
 app.use(function(req, res) {
-    res.status(404).render('404');
+    res.status(404).render('404', { title: '404' });
 });
 
 module.exports = app;
